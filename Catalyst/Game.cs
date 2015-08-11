e7dc0ee1e1bdc049dbfa1a9d618d983d105651b6
Matthew Hall <gameguy566@gmail.com>
2015-08-12 09:44:49 +1200

    Added the Game.cs class

---
+++ Catalyst/Game.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Catalyst
+{
+	public class Game
+	{
+		private string input;
+
+		public Game (){
+			//
+		}
+
+		void gameLoop(){
+			while (true) {
+				
+			}//end of game loop
+		}
+	}
+}
+

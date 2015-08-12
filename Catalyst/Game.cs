42b0f9014cadf3bf8a2c1d841268f725701e1462
Matthew Hall <gameguy566@gmail.com>
2015-08-12 14:13:43 +1200

    Added room enum, player struct and started user input processing

--- Catalyst/Game.cs
+++ Catalyst/Game.cs
@@ -4,16 +4,37 @@ namespace Catalyst
 {
 	public class Game
 	{
-		private string input;
+		private string userInput;
+		private string[] inputSplit;
+
+		//Every room in the game's number
+		private enum room{
+			Start = 0,
+			//the following rooms will be used for testing
+			WestOfStart = 1,
+			NorthOfStart = 2,
+			NorthWestOfStart = 3
+		}
+
+		//Data type that will hold player information
+		private struct player{
+			public room playerRoom; 
+		}
+
 
 		public Game (){
 			//
 		}
 
 		void gameLoop(){
+			//start of game loop
 			while (true) {
 				
 			}//end of game loop
+		}
+
+		private void processUserInput(){
+			inputSplit = userInput.Split ();
 		}
 	}
 }
